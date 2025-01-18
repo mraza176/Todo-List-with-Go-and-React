6504762ac8f4fc4e6f2e40e4dcc86a187800c99f
Muhammad Raza <144789342+mraza176@users.noreply.github.com>
2025-01-18 23:04:11 +0500

    chahge api url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,7 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+export const BASE_URL = "https://goginapi.vercel.app/api";
 
 function App() {
   return (
